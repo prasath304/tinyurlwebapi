0a4f21aee182039f9b97c14c25c8531fc5b45614
prasath <prasath304@gmail.com>
2026-03-08 12:59:08 +0530

    testing auto deployment

--- tinyurlwebapi/Program.cs
+++ tinyurlwebapi/Program.cs
@@ -15,7 +15,8 @@ if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-        c.RoutePrefix = string.Empty; 
+        c.RoutePrefix = "swagger"; 
+
     });
 
 }
