66dedfbc86dab2f1d77005b7b17e6bfb0d73d029
prasath <prasath304@gmail.com>
2026-03-08 12:35:34 +0530

    swagger json added in program file

--- tinyurlwebapi/Program.cs
+++ tinyurlwebapi/Program.cs
@@ -13,7 +13,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty; // <-- makes Swagger UI the default at "/"
+    });
+
 }
 
 app.UseHttpsRedirection();
